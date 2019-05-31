8c1f0d54f324426a6a7d74bbbfb3f69efa10e74f
kgogolaa <gogolinex@interia.pl>
2019-05-31 09:46:36 +0200

    Dodaj pliki projektów.

---
+++ DateScanner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
